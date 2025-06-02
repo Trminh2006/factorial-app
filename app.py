4ff4402e2096e30bb316731064c18b22d4b1fe7d
Trminh2006 <168347296+Trminh2006@users.noreply.github.com>
2025-06-02 16:02:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from factorial import fact
 def main():
     st.title("Factorial Calculator")
     number = st.number_input("Enter a number :", min_value =0, max_value = 900)
-    if st.button ("Calculate") :
+    if st.button ("Calculate"):
         result = fact(number)
     st.write(f"The factorial of {number} is {result}")
 
