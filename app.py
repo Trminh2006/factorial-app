e5679fb69f8ab5d2155007318587a197bb1a47e9
Trminh2006 <168347296+Trminh2006@users.noreply.github.com>
2025-06-02 16:02:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     number = st.number_input("Enter a number :", min_value =0, max_value = 900)
     if st.button ("Calculate") :
         result = fact(number)
-    st.write(f"The factorial of { number } is { result }")
+    st.write(f"The factorial of {number} is {result}")
 
 if __name__ == "__main__":
     main()
